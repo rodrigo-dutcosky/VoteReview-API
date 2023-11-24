795fa5b7613fbe151e6e10bc7d7b837d4fa95a7b
Rodrigo Dutcosky <rodrigo.dutcosky@gmail.com>
2023-11-24 19:44:46 -0300

    add folder projeto

---
+++ projeto/ibovescraper/script.py
@@ -0,0 +1,21 @@
+import os
+import bs4
+import json
+import requests
+import datetime
+
+ACOES = json.loads(open("acoes.json", "r").read())
+
+def salvar(obj:dict):
+    pass
+
+def buscar(obj:dict):
+    pass
+
+def scrape():
+    pass
+
+
+if __name__ == "__main__": 
+    scrape()
+    
